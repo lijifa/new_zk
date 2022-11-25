01fe99cbf63a521eee2e67c9cc13a50071805def
soonespresso <icanflylzx2012@163.com>
2022-11-25 10:56:14 +0800

    feat: 所在地（省/市/县区）联动选择器

--- src/components/ZKAddressCascader/index.tsx
+++ src/components/ZKAddressCascader/index.tsx
@@ -4,9 +4,10 @@
 
 import { getCity, getCounty, getProvince } from '@/services/SystemConfig/ProjectSetting/site'
 import { useMount } from 'ahooks'
-import { Button, Cascader, CascaderProps } from 'antd'
-import { isEmpty } from 'lodash'
-import { forwardRef, useEffect, useState } from 'react'
+import { Cascader } from 'antd'
+import { isEmpty, isNil } from 'lodash'
+import { useEffect, useState } from 'react'
+
 
 type Option = {
   value: string
@@ -18,14 +19,19 @@ type Option = {
   children?: Option[]
 }
 
-// type ZKAddressCascaderProps = Omit<React.PropsWithChildren<CascaderProps<Option>>, 'options' | 'loadData'>
+
+/**
+ * 所在地（省/市/县区）联动选择器
+ */
 type ZKAddressCascaderProps = {
   data: string[]
   onChange: (value: string[], selectOptions: Option[]) => void
+  placeholder?: React.ReactNode
 }
 // forwardRef
 const ZKAddressCascader: React.FC<ZKAddressCascaderProps> = ((props) => {
 
+  const [loading, setLoading] = useState(false)
   const [value, setValue] = useState<string[]>(props.data ?? [])
   
   const [options, setOptions] = useState<Option[]>([])
@@ -34,38 +40,69 @@ const ZKAddressCascader: React.FC<ZKAddressCascaderProps> = ((props) => {
     const targetOption = selectedOptions[selectedOptions.length - 1]
     switch (targetOption.level) {
       case 'province':
-        setCityOptions(targetOption)
+        if (isNil(targetOption.children)) {
+          getCityOptions(targetOption).finally(() => setOptions([...options]))
+        }
         break
       case 'city':
-        setCountyOptions(targetOption)
+        if (isNil(targetOption.children)) {
+          getCountyOptions(targetOption).finally(() => setOptions([...options]))
+        }
         break
     }
   }
 
-  async function setCountyOptions(cityOption: Option) {
+  async function getCountyOptions(cityOption: Option) {
     cityOption.loading = true
     const res = await getCounty(+cityOption.value).finally(() => cityOption.loading = false)
     if (res.code === '200') {
-      cityOption.children = formatOptions(res.data ?? [], 'county', cityOption.path)
-      console.log(options)
-      setOptions([...options])
+      return cityOption.children = formatOptions(res.data ?? [], 'county', cityOption.path)
     }
+    return []
   }
 
-  async function setCityOptions(provinceOption: Option) {
+  async function getCityOptions(provinceOption: Option) {
     provinceOption.loading = true
     const res = await getCity(+provinceOption.value).finally(() => provinceOption.loading = false)
     if (res.code === '200') {
-      provinceOption.children = formatOptions(res.data ?? [], 'city', provinceOption.path)
-      setOptions([...options])
+      return provinceOption.children = formatOptions(res.data ?? [], 'city', provinceOption.path)
     }
+    return []
   }
 
-  async function setProvinceOptions() {
+  async function getProvinceOptions() {
     const res = await getProvince()
     if (res.code === '200') {
-      setOptions(formatOptions(res.data ?? [], 'province'))
+      return formatOptions(res.data ?? [], 'province')
     }
+    return []
+  }
+
+  async function renewOptions() {
+    const [province, city, county] = props.data
+    let optionList: Option[] = options
+    if (isEmpty(options)) {
+      optionList = await getProvinceOptions()
+    }
+    // 省
+    if (province) {
+      const provinceOption = optionList.find(({ value }) => value === province)
+      // 市
+      if (provinceOption && city) {
+        let cityOptions: Option[] = []
+        if (isNil(provinceOption.children)) {
+          cityOptions = await getCityOptions(provinceOption)
+        }
+        const cityOption = cityOptions.find(({ value }) => value === city)
+        // 县
+        if (cityOption && county) {
+          if (isNil(cityOption.children)) {
+            await getCountyOptions(cityOption)
+          }
+        }
+      }
+    }
+    setOptions([...optionList])
   }
 
   function formatOptions(data: any[], level: Option['level'], basepath = '') {
@@ -85,34 +122,22 @@ const ZKAddressCascader: React.FC<ZKAddressCascaderProps> = ((props) => {
     )
   }
 
-  /* async function formatOptions() {
-  } */
-
   useEffect(() => {
     setValue(props.data ?? [])
   }, [props.data])
 
-  useMount(async () => {
-    await setProvinceOptions()
-    if (value.length > 1) {
-      const provinceOption = options.find(({ value: val }) => val === value[0])
-      if (provinceOption) {
-        await setCityOptions(provinceOption)
-      }
-      if (value.length > 2 && !isEmpty(provinceOption?.children)) {
-        const cityOption = provinceOption!.children!.find(({ value: val }) => val === value[2])
-        if (cityOption) {
-          await setCountyOptions(cityOption)
-        }
-      }
-    }
+  useMount(() => {
+    setLoading(true)
+    renewOptions().finally(() => setLoading(false))
   })
 
-  return <>
+  return (
     <Cascader
+      loading={loading}
       options={options}
       loadData={loadData as any}
       value={value}
+      placeholder={props.placeholder}
       onChange={
         (value: any, selectedOptions: any) => {
           setValue(value)
@@ -120,7 +145,7 @@ const ZKAddressCascader: React.FC<ZKAddressCascaderProps> = ((props) => {
         }
       }
     />
-  </>
+  )
 })
 
 export default ZKAddressCascader
